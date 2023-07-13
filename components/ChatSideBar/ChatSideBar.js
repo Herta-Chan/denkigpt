031d74a8815b45e4a4805c102c40314e857a0c8e
Herta-Chan <denkimcvn@gmail.com>
2023-07-14 05:08:14 +0700

    Test

---
+++ components/ChatSideBar/ChatSideBar.js
@@ -0,0 +1,49 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMessage, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export const ChatSideBar = ({ chatId }) => {
+  const [chatList, setChatList] = useState([]);
+    useEffect(() => {
+      const loadChatList = async () => {
+        const response = await fetch(`/api/chat/getChatList`, {
+          method: "POST",
+        });
+        const json = await response.json();
+        setChatList(json?.chats || []);
+      };
+      loadChatList();
+    }, [chatId]);
+    return (
+      <div className="flex flex-col overflow-hidden bg-gray-900 text-white">
+        <div className="bg-gray-800">
+        <Link
+          href="/chat"
+          className="side-menu-item bg-emerald-500 hover:bg-emerald-600"
+        >
+          <FontAwesomeIcon icon={faPlus} />
+          Chat Mới
+        </Link>
+        </div>
+        <div className="bg-grey-950 flex-1 overflow-auto sm:max-h-36">
+          {chatList.map((chat) => (
+            <Link
+              key={chat._id}
+              href={`/chat/${chat._id}`}
+              className={`side-menu-item ${chatId === chat._id ? "bg-gray-700 hover:bg-gray-700" : ""}`}
+            >
+              <FontAwesomeIcon icon={faMessage} />
+              <span title={chat.title} className="overflow-hidden text-ellipsis whitespace-nowrap">{chat.title}</span>
+            </Link>
+          ))}
+        </div>
+        <div className="bg-gray-700">
+        <Link className="side-menu-item" href="/api/auth/logout">
+          <FontAwesomeIcon icon={faRightFromBracket} />
+          Logout
+        </Link>
+        </div>
+      </div>
+    );
+};
---
+++ pages/index.js
@@ -0,0 +1,67 @@
+import Head from "next/head";
+import Link from "next/link";
+import { useUser } from '@auth0/nextjs-auth0/client'
+import { getSession } from '@auth0/nextjs-auth0'
+import { faCat } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function Home() {
+  const {isLoading, error, user} = useUser();
+
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>{error.message}</div>
+
+  return (
+    <>
+      <Head>
+        <title>DenkiGPT | Login | Signup</title>
+      </Head>
+      <div className="flex min-h-screen w-full items-center justify-center bg-gray-800 text-center text-white">
+        <div>
+          {!!user && <Link href="/api/auth/logout">Logout</Link>}
+          {!user && (
+            <>
+              <div className="my-3 text-5xl">
+                <FontAwesomeIcon icon={faCat} className="text-emerald-200" />
+              </div>
+              <div className="my-3 text-3xl">
+                <h1>Welcome to DenkiGPT</h1>
+              </div>
+              <div className="my-3 text-xl">
+                <h1>
+                  By{" "}
+                  <a
+                    href="https://discord.gg/folodu/"
+                    className="text-emerald-200 underline"
+                  >
+                    ItzDenki
+                  </a>
+                </h1>
+              </div>
+              <Link href="/api/auth/login" className="btn">
+                Login
+              </Link>
+              <Link href="/api/auth/signup" className="btn ml-2">
+                Signup
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export const getServerSideProps = async (ctx) => {
+  const session = await getSession(ctx.req, ctx.res);
+  if (!!session) {
+    return {
+      redirect: {
+        destination: "/chat",
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+};
